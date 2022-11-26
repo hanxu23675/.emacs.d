61d2f0e3ef0577353e663aac99c30b23c27447d3
Jordan Hx <51397412+hanxu23675@users.noreply.github.com>
2022-11-25 23:28:12 -0500

    Update init-startup.el

--- config/init-startup.el
+++ config/init-startup.el
@@ -1,20 +1,41 @@
 ;;; init-startup.el --- settings for Emacs startup.
 ;;; Commentary:
-
 ;;; Code:
-;(prefer-coding-system 'utf-8)
-;(set-default-coding-systems 'utf-8)
-;(set-terminal-coding-system 'utf-8)
-;(set-keyboard-coding-system 'utf-8)
-;(setq default-buffer-file-coding-system 'utf-8)
 
-;;; macOS
-;; move file to trash when delete
-(when (eq system-type 'darwin)
-  (setq delete-by-moving-to-trash t))
+;; Original value of package-archives:
+;; (("gnu" . "https://elpa.gnu.org/packages/")("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+;; Add MELPA repository
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
-;; do not show start screen
-(setq inhibit-startup-screen t)
+;; configure use-package prior to loading it
+(eval-and-compile
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t)
+  (setq use-package-expand-minimally t)
+  (require 'use-package))
+
+(use-package diminish)
+(use-package delight)
+
+(use-package company
+  :init
+  ;; To use company-mode in all buffers
+  (add-hook 'after-init-hook 'global-company-mode)
+  ;; Company do not work along with gud (debugging) mode for some reason
+  (add-hook 'gud-mode-hook (lambda() (company-mode 0)))
+  :config
+  (setq company-minimum-prefix-length 3
+        company-show-quick-access t))
+
+(use-package flycheck
+  :init (global-flycheck-mode)
+  :config (flycheck-popup-tip-mode))
+
 
 (provide 'init-startup)
 ;;; init-startup.el ends here
