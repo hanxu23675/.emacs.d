f59b9641a339f8438b5fe8300f546280558a4360
Jordan Hx <51397412+hanxu23675@users.noreply.github.com>
2022-11-25 23:25:16 -0500

    Update init.el

--- init.el
+++ init.el
@@ -2,18 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
+;; set the startup default directory, not essential but recommended.
+(setq default-directory "~/")
 ;; When Emacs is started, Emacs looks for your init file using the filenames ~/.emacs.el, ~/.emacs, or ~/.emacs.d/init.el in that order.
 ;; Then Emacs will set user-emacs-directory to the directory it decides to use.
-;; Update the load-path with the subdirectory of all configuration files.
-(add-to-list 'load-path (expand-file-name (concat user-emacs-directory "config/")))
+;; update load-path to make customized lisp codes work
+(dolist (folder (directory-files (concat user-emacs-directory "config") t directory-files-no-dot-files-regexp))
+  (add-to-list 'load-path folder))
 
 ;; Load configuration files (if it hasn't already been loaded)
 (require 'init-startup)
-(require 'init-elpa)
-(require 'init-package)
-(require 'init-ide)
 (require 'init-keybind)
 (require 'init-ui)
+(require 'init-cpp)
+(require 'init-py)
 
 ;; save customizations somewhere other than your initialization file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
